78fc0f2f30ac8fb3df9a56d6c18ddfef77335204
n2ttawut <nat_taw_ut001@hotmail.co.th>
2023-09-26 01:10:04 +0700

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,61 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  static token = '';
+  async getMenber(client_account) {
+    const accNum = Number(client_account.trim())
+    if (AppService.token !== '') {
+
+      return this.getPartnerMumber(accNum);
+    } else {
+      return await this.login().then(async (x) =>
+        this.getPartnerMumber(accNum)
+      );
+    }
+  }
+  login() {
+    const axios = require('axios');
+    const FormData = require('form-data');
+    let data = new FormData();
+    data.append('login', process.env.EMAIL_LOGIN);
+    data.append('password', process.env.PASSWORD_LOGIN);
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: process.env.URL_AUTH,
+      headers: {
+        'Authorization': 'Bearer AIzaSyDOTWEzC_xndro-TULSzOfKncmlTonhH9I',
+        ...data.getHeaders()
+      },
+      data: data
+    };
+    return axios.request(config)
+      .then((response) => {
+        AppService.token = response.data.token;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }
+  async getPartnerMumber(client_account) {
+    const axios = require('axios');
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: process.env.URL_CLIENTS + '?client_account=' + client_account + '&&limit=1&offset=0&ordering=-trade_fn',
+      headers: {
+        'Authorization': 'JWT ' + AppService.token,
+      }
+    };
+    const datasPartNer = await axios.request(config)
+      .then((response) => {
+        return response.data.totals.count;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return datasPartNer.toString();
+
   }
 }
