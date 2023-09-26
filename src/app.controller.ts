d94830cf0e51a8df8cdb5c5e74be04d5ba98db55
n2ttawut <nat_taw_ut001@hotmail.co.th>
2023-09-26 09:13:50 +0700

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,22 @@
 import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { find } from 'lodash';
+const user = require('../data/user.json');
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get('partner/')
-  getMenber(@Body() id) {
-
-    for (const key in id) {
-
-      return this.appService.getMenber(key.replace('\x00', ''));
+  getMenber(@Body() ids) {
+    for (const key in ids) {
+      let accOwner = find(user.account, { id: Number(key.replace('\x00', '')) })
+      let accPastner = find(user.data, { client_account: Number(key.replace('\x00', '')) })
+      if (accOwner || accPastner ) {
+        return 1;
+      }
+      else{
+        return this.appService.getMenber(key.replace('\x00', ''));
+      }
     }
   }
 }
